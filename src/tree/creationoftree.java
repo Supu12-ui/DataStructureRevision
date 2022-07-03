954de1d62c0ddf5b02f32e9f90c7723378911342
supriyam12 <supriya.m@goinfluencer.io>
2022-07-03 14:32:44 +0530

    new changes

--- src/tree/creationoftree.java
+++ src/tree/creationoftree.java
@@ -1,5 +1,31 @@
 package tree;
 
 public class creationoftree {
-    
+    public static void main(String [] args)
+    {
+       Node node =new Node(2);
+       node.left=new Node(3);
+       print(node);
+    }
+    static void print(Node node)
+    {
+        if(node==null)
+        {
+            return ;
+        }
+        print(node.left);
+        System.out.println(node.data);
+        print(node.right);
+    }
+}
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data=data;
+        left=right=null;
+    }
 }
